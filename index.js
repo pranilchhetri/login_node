ef87463e6a5f5728ba5690523071b0c271724028
pranilchhetri <pranilchhetri2014@gmail.com>
2019-04-16 14:03:29 +0545

    database crud

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var mysql      = require('mysql');
 var config = require('./config');
 let connection=mysql.createConnection(config);
 
-let sql = `INSERT INTO employee (employee_name,employee_salary,employee_age)
-            VALUES ('Pranil',50000,24)`;
-    connection.query(sql);
+// let sql = `INSERT INTO employee (employee_name,employee_salary,employee_age)
+//             VALUES ('Pranil',50000,24)`;
+    
+    // let sql=`UPDATE employee SET employee_name="William",employee_salary=500,employee_age=32 where employee_name="pranil"`;
+   
 
-    connection.end();
+connection.query(`SELECT * FROM employee`,function(error,results,fields){
+    if (error) throw error;
+    console.log('The response is: ',results);
+});
+
+connection.end();
 
     
